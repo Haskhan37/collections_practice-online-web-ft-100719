8eab9d42d9a286917c55db041ef0aeb500579b22
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-12 18:54:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -75,7 +75,15 @@ end
 
 
 
-
+def add_s_collect(array)
+      array.each_with_index.collect do |word,index|
+        if index != 1 
+          word = word + "s"
+        else
+          word = word
+        end
+  end
+end
 
 
 
